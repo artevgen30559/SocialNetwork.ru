3727fa56ed698b3ed80d36e712b465436b144be7
Artevgen <artevgen.work@gmail.com>
2020-04-05 19:14:13 +0700

    Create modal for register. Create navbar top

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Социальная</title>
+		<?php include('layouts/head.php'); ?>
+	</head>
+	<body>
+		<?php include('layouts/header.php'); ?>
+		<section class="welcome">
+			<h3 class="welcome-caption">Зарегистрируйтесь или авторизуйтесь, чтобы начать.</h3>
+		</section>
 
-print_r($_GET);
-echo "<br><br>";
-print_r($_SERVER['REQUEST_URI']);
+
+
+	</body>
+	<?php include('layouts/footer.php'); ?>
+</html>
---
+++ views/modalRegister.php
@@ -0,0 +1,35 @@
+<div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Регистрация аккаунта</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<form>
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" placeholder="Имя пользователя" aria-label="Имя пользователя" aria-describedby="basic-addon1">
+					</div>
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" placeholder="Логин" aria-label="Логин" aria-describedby="basic-addon1">
+					</div>
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" placeholder="Пароль" aria-describedby="basic-addon1">
+					</div>
+				</form>
+				<div class="alert alert-danger" role="alert">
+  					Это основное уведомление — check it out!
+				</div>
+				<div class="alert alert-success" role="alert">
+  					Это основное уведомление — check it out!
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+				<button type="button" class="btn btn-primary">Зарегистрироваться</button>
+			</div>
+		</div>
+	</div>
+</div>
